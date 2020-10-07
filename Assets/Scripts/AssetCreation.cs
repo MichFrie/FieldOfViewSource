9e5d0a731deb7ac7244b29d500ac7027112f8c11
MichFrie <mifrpost@gmail.com>
2020-10-07 22:10:09 +0200

    implemented cube movement

--- Assets/Scripts/AssetCreation.cs
+++ Assets/Scripts/AssetCreation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AssetCreation : MonoBehaviour
@@ -9,134 +10,184 @@ public class AssetCreation : MonoBehaviour
     int gameObjectLength = 4;
     public Transform[] cubes;
     int i = 0;
+    int randomNumber = 0;
+    float speed = 0.5f;
+
+    public GameObject cubesParent;
+    
     private void Awake()
     {
-        cubes = new Transform[4];
-        while (i < gameObjectLength)
-        {
-            //J();
-            //I();
-            //L();
-            //O();
-            //S();
-            //T();
-            Z();
-        }
+         cubesParent = GameObject.FindWithTag("Respawn");
+    }
+    private void Start()
+    {
+       
+    }
+    private void Update()
+    {
+        StartCoroutine("ChooseRandomElement");
+        transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime; 
     }
 
+    private IEnumerator ChooseRandomElement()
+    {
+        cubes = new Transform[4];
+        while (true)
+        {
+            randomNumber = UnityEngine.Random.Range(0, 6);
+            switch (randomNumber)
+            {
+                case 0: J(); break;
+                case 1: I(); break;
+                case 2: L(); break;
+                case 3: O(); break;
+                case 4: S(); break;
+                case 5: T(); break;
+                case 6: Z(); break;
+                default: Debug.Log("Choice not possible"); break;
+            }
+            yield return null;
+        }
+    }
     private void J()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        if (i == 0)
+        while (i < gameObjectLength)
         {
-            prefabPosition.localPosition = Vector3.right * i;
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            if (i == 0)
+            {
+                prefabPosition.localPosition = Vector3.right * i;
+            }
+            else if (i == 1)
+            {
+                prefabPosition.localPosition = Vector3.forward * i;
+            }
+            else
+            {
+                prefabPosition.localPosition = Vector3.right * (i - 1);
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        else if (i == 1)
-        {
-            prefabPosition.localPosition = Vector3.forward * i;
-        }
-        else
-        {
-            prefabPosition.localPosition = Vector3.right * (i - 1);
-        }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
+
     }
     private void I()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        prefabPosition.localPosition = (Vector3.right * i);
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
+        while (i < gameObjectLength)
+        {
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            prefabPosition.localPosition = (Vector3.right * i);
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
+        }
     }
 
     private void L()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        prefabPosition.localPosition = (Vector3.right * i);
-        if(i == 3)
+        while (i < gameObjectLength)
         {
-            prefabPosition.localPosition = (Vector3.right * (i - 1)) + (Vector3.forward * (i - 2));
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+
+            cubes[i] = prefab;
+            prefabPosition.localPosition = (Vector3.right * i);
+            if (i == 3)
+            {
+                prefabPosition.localPosition = (Vector3.right * (i - 1)) + (Vector3.forward * (i - 2));
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
     }
     private void O()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        if (i == 0)
+        while (i < 4)
         {
-            prefabPosition.localPosition = Vector3.right * i;
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            if (i == 0)
+            {
+                prefabPosition.localPosition = Vector3.right * i;
+            }
+            else if (i == 1)
+            {
+                prefabPosition.localPosition = Vector3.forward * i;
+            }
+            else if (i == 2)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 1)) + (Vector3.right * (i - 1));
+            }
+            else
+            {
+                prefabPosition.localPosition = (Vector3.right * (i - 2));
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        else if (i == 1)
-        {
-            prefabPosition.localPosition = Vector3.forward * i;
-        }
-        else if(i == 2)
-        {
-            prefabPosition.localPosition = (Vector3.forward * (i - 1)) + (Vector3.right * (i - 1));
-        }
-        else
-        {
-            prefabPosition.localPosition = (Vector3.right * (i - 2));
-        }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
+
     }
     void S()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        prefabPosition.localPosition = Vector3.right * i;
-        if (i == 2)
+        while (i < gameObjectLength)
         {
-            prefabPosition.localPosition = (Vector3.forward * (i - 1)) + (Vector3.right * (i - 1));
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            prefabPosition.localPosition = Vector3.right * i;
+            if (i == 2)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 1)) + (Vector3.right * (i - 1));
+            }
+            else if (i == 3)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 1));
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        else if(i == 3)
-        {
-            prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 1));
-        }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
+
     }
 
     private void T()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        prefabPosition.localPosition = Vector3.right * i;
-        if(i == 3)
+        while (i < gameObjectLength)
         {
-            prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 2));
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            prefabPosition.localPosition = Vector3.right * i;
+            if (i == 3)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 2));
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
+
     }
     private void Z()
     {
-        Transform prefabPosition = Instantiate(prefab);
-        cubes[i] = prefab;
-        if(i == 0)
+        while (i < gameObjectLength)
         {
-            prefabPosition.localPosition = Vector3.forward * (i + 1);
+            Transform prefabPosition = Instantiate(prefab, cubesParent.transform);
+            cubes[i] = prefab;
+            if (i == 0)
+            {
+                prefabPosition.localPosition = Vector3.forward * (i + 1);
+            }
+            else if (i == 1)
+            {
+                prefabPosition.localPosition = (Vector3.forward * i + Vector3.right * i);
+            }
+            else if (i == 2)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 1));
+            }
+            else if (i == 3)
+            {
+                prefabPosition.localPosition = (Vector3.forward * (i - 3) + Vector3.right * (i - 1));
+            }
+            prefabPosition.localScale = Vector3.one / 1.05f;
+            i++;
         }
-        else if(i == 1)
-        {
-            prefabPosition.localPosition = (Vector3.forward * i + Vector3.right * i);
-        }
-        else if(i == 2)
-        {
-            prefabPosition.localPosition = (Vector3.forward * (i - 2) + Vector3.right * (i - 1));
-        }
-        else if (i == 3)
-        {
-            prefabPosition.localPosition = (Vector3.forward * (i - 3) + Vector3.right * (i - 1));
-        }
-        prefabPosition.localScale = Vector3.one / 1.05f;
-        i++;
     }
 }
